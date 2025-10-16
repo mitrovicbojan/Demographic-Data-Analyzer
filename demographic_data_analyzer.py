119b1642ea98c5bac49395de420612c699fc6bb0
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-10-16 22:09:46 +0300

    imported data

---
+++ demographic_data_analyzer.py
@@ -0,0 +1,18 @@
+import numpy as np;
+import pandas as pd;
+
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+census_income = fetch_ucirepo(id=20) 
+  
+# data (as pandas dataframes) 
+X = census_income.data.features 
+y = census_income.data.targets 
+  
+# metadata 
+#print(census_income.metadata) 
+  
+# variable information 
+#print(census_income.variables) 
+print(y)
