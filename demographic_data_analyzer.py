04bfec007f91c62c0de04b7b9ec132e545aa6925
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-10-18 17:13:16 +0300

    calc age and race count

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -15,4 +15,29 @@ y = census_income.data.targets
   
 # variable information 
 #print(census_income.variables) 
-print(y)
+
+#How many people of each race are represented in this dataset? 
+#This should be a Pandas series with race names as the index labels.
+
+# Get count of each unique race
+race_counts = X['race'].value_counts().reset_index()
+
+# Rename columns for clarity
+race_counts.columns = ['race', 'count']
+
+'''
+# Add percentage of each race representatives 
+# Add percentage column
+race_counts['percentage'] = (race_counts['count'] / race_counts['count'].sum()) * 100 
+
+# Optionally round percentages
+race_counts['percentage'] = race_counts['percentage'].round(2)
+
+print(race_counts)
+'''
+
+#What is the average age of men?
+male_rows = X[X['sex'] == 'Male']
+
+male_avg_age = male_rows['age'].mean().round(2)
+print(male_avg_age)
