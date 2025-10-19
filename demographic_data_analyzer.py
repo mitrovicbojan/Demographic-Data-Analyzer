2151e9efe02cf356689f764e2446c421301c168f
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-10-19 19:38:11 +0300

    calc high income per high edu

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -52,4 +52,38 @@ bs_deg_holder = edu_counts[edu_counts['education'] == 'Bachelors']['count'].sum(
 
 bs_deg_perc = (bs_deg_holder / edu_counts['count'].sum()) * 100 
 
-print("Percentage of people who have a Bachelor's degree is:", bs_deg_perc.round(2),"%")
+#print("Percentage of people who have a Bachelor's degree is:", bs_deg_perc.round(2),"%")
+
+#What percentage of people with advanced education (Bachelors, Masters, or Doctorate) make more than 50K?
+
+df = pd.concat([X, y], axis=1)
+
+# clean dot in income column
+df['income'] = df['income'].str.replace('.', '', regex=False).str.strip()
+bs_nums = df[['education', 'income']].value_counts().reset_index(name='count')
+
+#(cond1) & (cond2) — both conditions must be in parentheses, 
+#otherwise Python tries to apply & between mismatched types (like Series and strings)
+
+ttl_num_bachelors = bs_nums[bs_nums['education'] == 'Bachelors']['count'].sum()
+high_income_bachelors = bs_nums[(bs_nums['education'] == 'Bachelors') & (bs_nums['income'] == '>50K')]['count'].sum()
+
+bs_with_higher_income = (high_income_bachelors / ttl_num_bachelors) * 100
+
+#print(bs_with_higher_income.round(2))
+
+high_edu_nums = bs_nums[
+    bs_nums['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
+]['count'].sum()
+
+high_edu_high_income = bs_nums[
+    bs_nums['education'].isin(['Bachelors', 'Masters', 'Doctorate']) &
+    (bs_nums['income'] == '>50K')
+]['count'].sum()
+
+res_high_income_edu = (high_edu_high_income / high_edu_nums) * 100
+
+print(res_high_income_edu.round(2))
+
+#What percentage of people without advanced education make more than 50K?
+#note: write a function for this one
